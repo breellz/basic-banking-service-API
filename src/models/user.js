9ca7222aed07987d7539a290b8c1dd4f94fa3bdc
Bassit owolabi <barseetbrown@gmail.com>
2021-12-26 10:41:06 +0100

    fixes

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const Transaction = require('../models/transaction')
+const Transaction = require('./transaction')
 const ObjectID = mongoose.Schema.Types.ObjectId
 
 const userSchema = new mongoose.Schema({
